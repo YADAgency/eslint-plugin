17b042ea718bddf471365698fadbdd6993db63f5
Matthieu Harlé <bonjour@matthieuharle.com>
2020-02-11 23:43:22 +0100

    Add a whitelist to the styled-components-ensure-native rule

--- tests/lib/rules/styled-components-ensure-units.js
+++ tests/lib/rules/styled-components-ensure-units.js
@@ -27,7 +27,7 @@ ruleTester.run('styled-components-ensure-units', rule, {
   invalid: [
     {
       code:
-        'const CustomView = styled.View`\n position: absolute;\n margin-right: 0;\n margin: 30;\n width: 100; padding: -10;\n`',
+        'const CustomView = styled.View`\n position: absolute;\n margin-right: 0;\n margin: 30;\n width: 100;\n padding: -10;\n`',
       errors: [
         {
           message: 'Property declaration `margin: 30;` is missing its unit.',
@@ -39,6 +39,20 @@ ruleTester.run('styled-components-ensure-units', rule, {
         },
         {
           message: 'Property declaration `padding: -10;` is missing its unit.',
+          type: 'TemplateElement',
+        },
+      ],
+    },
+    {
+      code:
+        'const StyledAdviceFAB = styled(Animated.createAnimatedComponent(FAB))`\n position: absolute;\n right: 16;\n bottom: 75;\n`',
+      errors: [
+        {
+          message: 'Property declaration `right: 16;` is missing its unit.',
+          type: 'TemplateElement',
+        },
+        {
+          message: 'Property declaration `bottom: 75;` is missing its unit.',
           type: 'TemplateElement',
         },
       ],
